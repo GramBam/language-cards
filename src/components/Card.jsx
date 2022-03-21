f51ccaf7d086dfed0aefa4f1558b6bc80dfc79e2
Graham <graham.moss@hotmail.com>
2022-03-21 15:41:36 -0400

    add button and mandarin-characters functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,43 @@
 import { useState, useEffect } from 'react'
+import { FaCheck, FaTimes } from 'react-icons/fa'
 
 function Card({ translation }) {
-  const { english, mandarin } = translation
+  const { english, mandarin, characters } = translation
 
   const [text, setText] = useState(english)
-  const [visibility, setVisibility] = useState('visible')
-  const [clickable, setClickable] = useState('enabled')
+  const [textVisible, setTextVisible] = useState('visible')
+  const [buttonVisible, setButtonVisible] = useState('hidden')
+  const [clickable, setClickable] = useState(true)
 
   const fade = () => {
-    setClickable('disabled')
-    setVisibility('hidden')
+    setClickable(false)
+    setTextVisible('hidden')
     setTimeout(() => fadeIn(), 750)
   }
 
   const fadeIn = () => {
     setText(mandarin)
-    setVisibility('visible')
+    setTextVisible('visible')
+    setButtonVisible('visible')
   }
 
   const onClick = () => {
-    fade()
+    if (clickable) {
+      fade()
+    }
   }
 
   return (
-    <div className={'card ' + clickable} onClick={onClick} >
-      <p className={'card-text ' + visibility} >{text}</p>
+    <div className='card-container' onClick={onClick}>
+      <div className='card' >
+        <p className={'card-text ' + textVisible} >{text}</p>
+        {characters ? <p className={'card-text characters ' + textVisible}>{text === english ? '' : characters}</p> : null}
+        <button className={'card-button check ' + buttonVisible}><FaCheck /></button>
+        <button className={'card-button cross ' + buttonVisible}><FaTimes /></button>
+      </div>
+
     </div>
+
   )
 }
 
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 import { translations } from '../data'
 
 function CardList() {
-  let randomOrder = translations.sort((a, b) => 0.5 - Math.random())
+  let randomOrder = translations.sort(() => 0.5 - Math.random())
   return (
     <div className="container">
       {[...Array(randomOrder.length)].map((_, i) => <Card key={i} translation={randomOrder[i]} />)}
