44b661935b06638718f6c0c3ee1e3921dfba2b14
Graham <graham.moss@hotmail.com>
2022-03-30 21:57:29 -0400

    quick responsiveness change

--- src/components/CatagoryPick.jsx
+++ src/components/CatagoryPick.jsx
@@ -1,8 +1,8 @@
 function CatagoryPick({ title, callback }) {
   return (
     <div>
-      <label htmlFor={title.toLowerCase()}>{title}</label>
-      <input type="checkbox" id={title.toLowerCase()} onChange={callback} defaultChecked={title === "Other"} />
+      <label htmlFor={title.toLowerCase()}>{title.charAt(0).toUpperCase() + title.slice(1)}</label>
+      <input type="checkbox" id={title.toLowerCase()} onChange={callback} defaultChecked={title === "other"} />
     </div>
   )
 }
